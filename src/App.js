4eb5ec54021f146af058adf87f235f37f64a8054
BrickBeard <brickbeard.io@gmail.com>
2018-09-21 17:02:36 -0500

    Working with stateand applying a click handler to change it.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Brandon", age: 28 },
+      { name: "Manu", age: 25 },
+      { name: "Abigail", age: 30 }
+    ],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = () => {
+    //console.log("Was clicked!");
+    // DON'T DO THIS!!!  this.state.persons[0].name = "BrickBeard";
+    this.setState({
+      persons: [
+        { name: "BrickBeard", age: 28 },
+        { name: "Manu", age: 22 },
+        { name: "Abigail", age: 29 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm BrickBeard</h1>
         <img src={bricklogo} alt="BrickBeard" />
         <p>This is really working!</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement("div", null, React.createElement("h1", { className: "App" }, "Hi, I'm BrickBeard"));
