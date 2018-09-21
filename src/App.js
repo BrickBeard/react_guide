79bc0e80ccc3c3f42504f08da2663378360d1fce
BrickBeard <brickbeard.io@gmail.com>
2018-09-21 16:29:38 -0500

    Understanding components more clearly.  About to update Person component with added functionality.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import bricklogo from "./images/Brick.png";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm BrickBeard</h1>
         <img src={bricklogo} alt="BrickBeard" />
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
+    // return React.createElement("div", null, React.createElement("h1", { className: "App" }, "Hi, I'm BrickBeard"));
   }
 }
 
